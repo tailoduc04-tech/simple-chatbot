aa20434be3c8113c34d0ebddce0998b14f986c9a
tailoduc04-tech <tai.loduc04@gmail.com>
2025-09-16 15:58:45 +0700

    Basic logic for main file

--- start.py
+++ start.py
@@ -1,14 +1,26 @@
-from google import genai
 from dotenv import load_dotenv
+from vector_database_management import load_document, split_document, load_vector_store, create_vector_store
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import os
+import config
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-client = genai.Client()
-
-result = client.models.embed_content(
-        model="gemini-embedding-001",
-        contents="What is the meaning of life?")
-
-print(result.embeddings)
+if __name__ == "__main__":
+    document = load_document(config.DOCUMENT_PATH)
+    print(f"Loaded document with length {len(document[0].page_content)}")
+    split_up_document = split_document(document)
+    print(f"Split up document into {len(split_up_document)} seperate documents")
+    embeddings_model = GoogleGenerativeAIEmbeddings(model=config.EMBEDDINGS_MODEL)
+    vector_store = create_vector_store(
+            split_up_document, 
+            embeddings_model,
+            config.VECTOR_DB_COLLECTION, 
+            config.VECTOR_DB_PATH
+            ) if not os.path.exists(
+                    os.path.join(config.VECTOR_DB_PATH, config.VECTOR_DB_COLLECTION)
+                                ) else load_vector_store(
+                                        embeddings_model,
+                                        config.VECTOR_DB_COLLECTION, 
+                                        config.VECTOR_DB_PATH)
--- vector_database_management.py
+++ vector_database_management.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
+import time
 
 def load_document(markdown_path):
     return UnstructuredMarkdownLoader(markdown_path).load()
@@ -10,17 +11,33 @@ def split_document(document):
         chunk_size=1000,
         chunk_overlap=200,
         add_start_index=True,
-    ).split_document(document)
+    ).split_documents(document)
 
 def create_vector_store(split_up_doc, embeddings_model, collection_name, db_path):
-    return Chroma.from_documents(
-        documents=split_up_doc,
-        embedding=embeddings_model,
-        collection_name=collection_name,
+    print("Creating vector store")
+    
+    vector_store = Chroma(
         persist_directory=db_path,
+        embedding_function=embeddings_model,
+        collection_name=collection_name
     )
+    
+    # Xu ly rate limit cua GoogleStudio
+    batch_size = 90
+    total_texts = len(split_up_doc)
+    
+    for i in range(0, total_texts, batch_size):
+        print(f"Processing batch {i  // batch_size}/ {total_texts // batch_size}")
+        batch = split_up_doc[i:i+batch_size]
+        vector_store.add_documents(batch)
+        
+        if i * batch_size < total_texts:
+            time.sleep(60)
+        
+    return vector_store
 
 def load_vector_store(embeddings_model, collection_name, db_path):
+    print("Loading vector store")
     return Chroma(
         persist_directory=db_path,
         embedding_function=embeddings_model,
