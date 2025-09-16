28d4570e091deb431601f67acb1ee8b77e85ab6a
Tai225999 <159406586+Tai225999@users.noreply.github.com>
2025-09-16 20:44:13 +0700

    Refactored main file for better perfomance

--- start.py
+++ start.py
@@ -8,19 +8,34 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
-    document = load_document(config.DOCUMENT_PATH)
-    print(f"Loaded document with length {len(document[0].page_content)}")
-    split_up_document = split_document(document)
-    print(f"Split up document into {len(split_up_document)} seperate documents")
-    embeddings_model = HuggingFaceEmbeddings(model_name=config.EMBEDDINGS_MODEL, model_kwargs={'device': 'cpu'})
-    vector_store = create_vector_store(
-            split_up_document, 
-            embeddings_model,
-            config.VECTOR_DB_COLLECTION, 
-            config.VECTOR_DB_PATH
-            ) if not os.path.exists(
-                    os.path.join(config.VECTOR_DB_PATH, config.VECTOR_DB_COLLECTION)
-                                ) else load_vector_store(
-                                        embeddings_model,
-                                        config.VECTOR_DB_COLLECTION, 
-                                        config.VECTOR_DB_PATH)
+     embedding_models = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL_NAME, model_kwargs={"device": "cpu"})
+
+     vector_store = None
+
+     # Kiểm tra xem thư mục db_path có tồn tại không, nếu không thì tạo mới csdl
+     if not os.path.exists(config.DB_PATH):
+        os.makedirs(config.DB_PATH)
+
+        # Load document từ file markdown
+        document = load_document(config.MARKDOWN_PATH)
+
+        # Chia nhỏ document
+        split_up_doc = split_document(document)
+
+        # Tạo vector store và lưu trữ
+        vector_store = create_vector_store(
+            split_up_doc, 
+            embedding_models, 
+            config.COLLECTION_NAME, 
+            config.DB_PATH)
+
+     else:
+        # Nếu thư mục db_path đã tồn tại, tải vector store từ thư mục
+        vector_store = load_vector_store(
+            embedding_models, 
+            config.COLLECTION_NAME, 
+            config.DB_PATH)
+
+     # Kiểm tra vector_store đã được khởi tạo chưa
+     assert vector_store is not None, "Vector store should be initialized"
+        
