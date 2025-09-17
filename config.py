8febfd53aaefa28d69ca5d1e65b39f986197c1ba
Tai225999 <159406586+Tai225999@users.noreply.github.com>
2025-09-17 07:26:52 +0700

    RAG functionality working

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+TEMPERATURE = 0.3
 LLM_MODEL="gemini-2.5-flash"
 EMBEDDINGS_MODEL="sentence-transformers/all-MiniLM-L6-v2"
 DOCUMENT_PATH="Document/n8n_docs_combined.md"
--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from vector_database_management import load_document, split_document, load_vector_store, create_vector_store
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI
+from rag_chain import RAGChain
 import os
 import config
 
@@ -8,16 +10,16 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 if __name__ == "__main__":
-     embedding_models = HuggingFaceEmbeddings(model_name=config.EMBEDDING_MODEL_NAME, model_kwargs={"device": "cpu"})
+    embedding_models = HuggingFaceEmbeddings(model_name=config.EMBEDDINGS_MODEL, model_kwargs={"device": "cpu"})
 
-     vector_store = None
+    vector_store = None
 
-     # Kiểm tra xem thư mục db_path có tồn tại không, nếu không thì tạo mới csdl
-     if not os.path.exists(config.DB_PATH):
-        os.makedirs(config.DB_PATH)
+    # Kiểm tra xem thư mục db_path có tồn tại không, nếu không thì tạo mới csdl
+    if not os.path.exists(config.VECTOR_DB_PATH):
+        os.makedirs(config.VECTOR_DB_PATH)
 
         # Load document từ file markdown
-        document = load_document(config.MARKDOWN_PATH)
+        document = load_document(config.D)
 
         # Chia nhỏ document
         split_up_doc = split_document(document)
@@ -26,18 +28,36 @@ if __name__ == "__main__":
         vector_store = create_vector_store(
             split_up_doc, 
             embedding_models, 
-            config.COLLECTION_NAME, 
-            config.DB_PATH)
+            config.VECTOR_DB_COLLECTION, 
+            config.VECTOR_DB_PATH)
 
-     else:
+    else:
         # Nếu thư mục db_path đã tồn tại, tải vector store từ thư mục
         vector_store = load_vector_store(
             embedding_models, 
-            config.COLLECTION_NAME, 
-            config.DB_PATH)
+            config.VECTOR_DB_COLLECTION, 
+            config.VECTOR_DB_PATH)
 
-     # Kiểm tra vector_store đã được khởi tạo chưa
-     assert vector_store is not None, "Vector store should be initialized"
-     
-     
-        
+    # Kiểm tra vector_store đã được khởi tạo chưa
+    assert vector_store is not None, "Vector store should be initialized"
+    
+    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k":5})
+    llm = ChatGoogleGenerativeAI(model=config.LLM_MODEL, temperature=config.TEMPERATURE)
+    
+    rag_chain = RAGChain(llm=llm, retriever=retriever)
+    app = rag_chain.build_graph()
+    
+    # Chạy ứng dụng
+    question = "How can I use n8n to automate tasks?"
+    print(f"Câu hỏi: {question}")
+    
+    inputs = {"question": question}
+    for output in app.stream(inputs, {"recursion_limit": 5}):
+        for key, value in output.items():
+            print(f"Kết quả từ nút: {key} là: {value}")
+
+    final_state = value
+    print("--------------------------------------------------")
+    print(f"Câu trả lời cuối cùng: {final_state.get('answer', 'Không có câu trả lời được tạo ra.')}")
+    print("--------------------------------------------------")
+    
